e71151e52be8d4f4c1aff165203768085b47cfc6
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2023-02-18 10:27:27 -0300

    aula4

---
+++ AULA 04/js/funcoes.js
@@ -0,0 +1,21 @@
+function VerificaForm(){
+    alert('VC CLICOU');
+    const senha = document.getElementById('exampleFormControlTextarea1');
+    const email = document.getElementById('exampleFormControlInput1');
+    const emailvalor = email.value;
+    const senhavalor = senha.value;
+
+    if(VerificaEmail(emailvalor)){
+
+        email.style.border = "2px solid green";
+    }else{
+        email.style.border = "2px solid red";
+    }
+    console.log(emailvalor);
+    console.log(senhavalor);
+
+}
+function VerificaEmail(emailvalor){
+    const indice = emailvalor.indexOf('@');
+    return indice !==-1;
+}
